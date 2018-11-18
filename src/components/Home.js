505509082f504ddc2af852e1fe70ee19c1eb5dbe
SarKurd <sarbast2009@hotmail.co.uk>
2018-11-18 18:05:50 +0100

    connected the api and fixed many bugs

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,15 @@
 import React from "react";
-import { setMembershipID } from "../actions/playerActions";
+import {
+  setMembershipDataAction as setMembershipData,
+  resetTheStateAction as resetTheState
+} from "../actions/playerActions";
 import { connect } from "react-redux";
-import { Redirect } from "react-router";
 
 class Home extends React.Component {
+  componentWillMount() {
+    this.props.resetTheState();
+  }
+
   setActive = (e, blur) => {
     if (blur && e.target.value.length === 0) {
       e.target.parentNode.classList.remove("active");
@@ -12,22 +18,27 @@ class Home extends React.Component {
     e.target.parentNode.classList.add("active");
   };
 
-  handlePress = event => {
+  handlePress = async event => {
     if (event.key === "Enter") {
       const id = event.target.value.toLowerCase();
-      this.props.setMembershipID(id);
+      try {
+        await this.props.setMembershipData(id);
+        this.props.history.push(`/player/${this.props.player.displayName}`);
+      } catch (err) {
+        console.log(err);
+      }
     }
   };
 
   render() {
-    const { success, error } = this.props.player;
-    if (success) {
-      return <Redirect to={`/player/xxsarkurdzz`} />; //the actual gamertag will be fetched from API but for now it's my gamertag
-    }
+    const { error } = this.props.player;
+    const errorPopup = (
+      <div className="error_popup">This player doesn't exist</div>
+    );
 
     return (
       <div className="home-wrapper">
-        {error && <div className="error_popup">This player doesn't exist</div>}
+        {error && errorPopup}
         <div className="input-container">
           <label htmlFor="gamertag">Enter a player's name</label>
           <input
@@ -50,15 +61,7 @@ const mapStoreToProps = store => {
   };
 };
 
-const mapDispatchToProps = dispatch => {
-  return {
-    setMembershipID: id => {
-      dispatch(setMembershipID(id));
-    }
-  };
-};
-
 export default connect(
   mapStoreToProps,
-  mapDispatchToProps
+  { setMembershipData, resetTheState }
 )(Home);
