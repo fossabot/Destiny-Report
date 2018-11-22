2800d20ed735dda49be797745cf9d5076b759d78
SarKurd <sarbast2009@hotmail.co.uk>
2018-11-22 16:23:04 +0100

    Removed Logger for production

--- src/store/store.js
+++ src/store/store.js
@@ -3,10 +3,15 @@ import { playerReducer } from "../reducers/playerReducer";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 
+let middleware = [thunk];
+if (process.env.NODE_ENV !== "production") {
+  middleware = [...middleware, logger];
+}
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   combineReducers({ player: playerReducer }),
-  composeEnhancers(applyMiddleware(thunk, logger))
+  composeEnhancers(applyMiddleware(...middleware))
 );
 
 export default store;
