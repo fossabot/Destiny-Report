4bf0518eae8578311316de8c7fa13639554ee760
SarKurd <sarbast2009@hotmail.co.uk>
2018-11-26 19:55:10 +0100

    Added donate button

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,11 @@ export default () => (
   <footer className="footer">
     <ul>
       <li>
+        <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=CBDESK5LVTJPC&source=url">
+          <i className="fa fa-paypal" />
+        </a>
+      </li>
+      <li>
         <a href="https://www.github.com/sarkurd">
           <i className="fa fa-github" />
         </a>
