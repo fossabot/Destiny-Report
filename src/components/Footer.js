125024eec7eff6dffe58d7e6db2a3ba2ae0800d8
SarKurd <sarbast2009@hotmail.co.uk>
2018-11-27 02:11:42 +0100

    Added license and contact info

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,15 @@ import React from "react";
 
 export default () => (
   <footer className="footer">
-    <ul>
+    <ul className="developer">
+      <li>
+        Developed by{" "}
+        <a className="developer-anchor" href="https://www.github.com/sarkurd">
+          SarKurd
+        </a>
+      </li>
+    </ul>
+    <ul className="source-and-contact">
       <li>
         <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=CBDESK5LVTJPC&source=url">
           <i className="fa fa-paypal" />
@@ -17,6 +25,12 @@ export default () => (
         <a href="https://twitter.com/xXSARKURDZz">
           <i className="fa fa-twitter" />
         </a>
+      </li>
+    </ul>
+    <ul className="info">
+      <li>
+        Contact:{" "}
+        <a href="mailto:info@destinyreport.app">info@destinyreport.app</a>
       </li>
     </ul>
   </footer>
