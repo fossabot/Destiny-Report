201278ff003ea543904b7fc36dcdb7acebbd05d2
SarKurd <sarbast2009@hotmail.co.uk>
2018-12-17 08:21:42 +0100

    Solving serving static files issue

--- main.js
+++ main.js
@@ -7,28 +7,26 @@ const cors = require("cors");
 const axios = require("axios");
 const { getCharactersIds } = require("./utility/endpoints");
 const Player = require("./models/player");
-
 const checkForBadges = require("./controllers/checkForBadges");
 
 const app = express();
 
-if (process.env.NODE_ENV === "production") {
-  const whitelist = ["https://destinyreport.app", "https://destiny.report"];
-  const corsOptions = {
-    origin: function(origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true
-  };
-  app.use(cors(corsOptions));
-} else {
-  app.use(cors());
-}
-const PORT = process.env.PORT || 8080;
+// if (process.env.NODE_ENV === "production") {
+//   const whitelist = ["https://destinyreport.app", "https://destiny.report"];
+//   const corsOptions = {
+//     origin: function(origin, callback) {
+//       if (whitelist.indexOf(origin) !== -1) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     credentials: true
+//   };
+//   app.use(cors(corsOptions));
+// } else {
+app.use(cors());
+//}
 axios.defaults.headers.common["X-API-KEY"] = process.env.BUNGIE_API_KEY;
 
 mongoose.connect(
@@ -38,6 +36,7 @@ mongoose.connect(
   { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false }
 );
 
+//Serve client
 app.use(express.static(path.join(__dirname, "client/build")));
 
 app.get("/api/getbadges/:id", async (req, res) => {
@@ -76,17 +75,15 @@ app.get("/api/checkbadges/:id", async (req, res) => {
         res.status(200).json(doc);
       }
     });
-    console.log("Done");
   } catch (err) {
-    res.status(500).json({ message: "something went wrong" });
-    console.log(err);
+    res.status(500).json(err);
   }
 });
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
+// The "catchall" handler
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
