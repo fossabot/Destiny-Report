d898ab092f270dc2fb43314b6deabf6f6062db1f
SarKurd <sarbast2009@hotmail.co.uk>
2018-12-31 21:53:26 +0100

    Applied the progress bar to other pages nad fixed a bug

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,75 +3,102 @@ import { NavLink, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import ProgressBar from "./ProgressBar";
 
-const navBar = props => {
-  let displayName = props.location.pathname.split("/")[2];
-  if (displayName === "") {
+class NavBar extends React.Component {
+  showProgressBarToggle = value => {
+    this.setState({ showProgressBar: value });
+  };
+
+  render() {
+    const path = this.props.location.pathname;
+    let showProgressBar = true;
+    const {
+      isApiLoading,
+      gambitIsLoading,
+      crucibleIsLoading,
+      raidIsLoading
+    } = this.props.player;
     if (
-      props.player.activeMembership !== -1 &&
-      props.player.memberships.length !== 0
+      (path.includes("raid") && (isApiLoading || raidIsLoading)) ||
+      (path.includes("crucible") && crucibleIsLoading) ||
+      ((path.includes("gambit") || path === "/") && gambitIsLoading)
     ) {
-      displayName =
-        props.player.memberships[props.player.activeMembership].displayName;
+      if (showProgressBar !== true) {
+        showProgressBar = true;
+      }
+    } else {
+      if (showProgressBar !== false) {
+        showProgressBar = false;
+      }
     }
-  }
-  const path = props.location.pathname;
-  const linkHide = path === "/" ? "link--hide" : "";
 
-  let { error, privacyError, errorMessage, isApiLoading } = props.player;
+    let displayName = this.props.location.pathname.split("/")[2];
+    if (displayName === "") {
+      if (
+        this.props.player.activeMembership !== -1 &&
+        this.props.player.memberships.length !== 0
+      ) {
+        displayName = this.props.player.memberships[
+          this.props.player.activeMembership
+        ].displayName;
+      }
+    }
+    const linkHide = path === "/" || path.includes("pgcr") ? "link--hide" : "";
 
-  const isRaidPage = path.includes("raid") ? true : false;
-  if (privacyError) {
-    errorMessage =
-      "Due to player's privacy, you can't see most of his/her stats";
-  }
-  const apiMaintenance = <div className="api-issue">{errorMessage}</div>;
+    let { error, privacyError, errorMessage } = this.props.player;
 
-  return (
-    <div className="navbar-wrapper">
-      <div className="navbar">
-        <ul className="navbar--home-item">
-          <li>
-            <NavLink exact to="/" className="link" replace={path === "/"}>
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to={`/gambit/${displayName}`}
-              activeClassName="link-active"
-              className={`link ${linkHide}`}
-              replace={path === `/gambit/${displayName}`}
-            >
-              Gambit
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to={`/crucible/${displayName}`}
-              activeClassName="link-active"
-              className={`link ${linkHide}`}
-              replace={path === `/crucible/${displayName}`}
-            >
-              Crucible
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to={`/raid/${displayName}`}
-              activeClassName="link-active"
-              className={`link ${linkHide}`}
-              replace={path === `/raid/${displayName}`}
-            >
-              Raid
-            </NavLink>
-          </li>
-        </ul>
+    if (privacyError) {
+      errorMessage =
+        "Due to player's privacy, you can't see most of his/her stats";
+    }
+    const ErrorBlock = <div className="api-issue">{errorMessage}</div>;
+
+    return (
+      <div className="navbar-wrapper">
+        <div className="navbar">
+          <ul className="navbar--home-item">
+            <li>
+              <NavLink exact to="/" className="link" replace={path === "/"}>
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to={`/gambit/${displayName}`}
+                activeClassName="link-active"
+                className={`link ${linkHide}`}
+                replace={path === `/gambit/${displayName}`}
+              >
+                Gambit
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to={`/crucible/${displayName}`}
+                activeClassName="link-active"
+                className={`link ${linkHide}`}
+                replace={path === `/crucible/${displayName}`}
+              >
+                Crucible
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to={`/raid/${displayName}`}
+                activeClassName="link-active"
+                className={`link ${linkHide}`}
+                replace={path === `/raid/${displayName}`}
+              >
+                Raid
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+        {showProgressBar && <ProgressBar />}
+        {(error || privacyError) && ErrorBlock}
       </div>
-      {isRaidPage && isApiLoading && <ProgressBar />}
-      {(error || privacyError) && apiMaintenance}
-    </div>
-  );
-};
+    );
+  }
+}
 
 const mapStoreToProps = store => {
   return {
@@ -79,4 +106,4 @@ const mapStoreToProps = store => {
   };
 };
 
-export default withRouter(connect(mapStoreToProps)(navBar));
+export default withRouter(connect(mapStoreToProps)(NavBar));
