748d362e37f8a4f3397c60efac75a8b33faf4f42
SarKurd <sarbast2009@hotmail.co.uk>
2019-02-13 19:30:25 +0100

    PORT fix3

--- src/index.js
+++ src/index.js
@@ -14,14 +14,16 @@ if (module.hot) {
   console.info("✅  Server-side HMR Enabled!");
 }
 
-const PORT = process.env.PORT || 4000;
-console.log(PORT);
+// const PORT = process.env.PORT || 4000;
+// console.log(PORT);
+
+const getEnv = c => process.env[c];
 export default express()
   .use((req, res) => app.handle(req, res))
-  .listen(PORT, function(err) {
+  .listen(getEnv("PORT") || 3000, function(err) {
     if (err) {
       console.error(err);
       return;
     }
-    console.log(`> Started server ${PORT}`);
+    console.log(`> Started server ${getEnv("PORT")}`);
   });
