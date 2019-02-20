42c13fca9f42ed4652284905a568cd386e82ef13
SarKurd <Sarbast2009@hotmail.co.uk>
2019-02-20 12:07:46 +0100

    Code splitting with React-loadable

--- src/common/components/Loading.js
+++ src/common/components/Loading.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 const Loading = () => {
-  return (
-    <div className="la-ball-clip-rotate la-3x">
-      <div />
-    </div>
-  );
+	return (
+		<div className="loading--wrapper">
+			<div className="la-ball-clip-rotate la-3x">
+				<div />
+			</div>
+		</div>
+	);
 };
 
 export default Loading;
--- src/common/containers/App.js
+++ src/common/containers/App.js
@@ -1,36 +1,64 @@
 import React from "react";
 import { Route, Switch } from "react-router";
 import NavBar from "../components/NavBar";
-import Home from "../components/Home";
+// import Home from "../components/Home";
 import Footer from "../components/Footer";
-import Gambit from "../components/Gambit";
-import Crucible from "../components/Crucible";
-import Raid from "../components/Raid";
-import PGCR from "../components/PGCR";
-import Error from "../components/Error";
+// import Gambit from "../components/Gambit";
+// import Crucible from "../components/Crucible";
+// import Raid from "../components/Raid";
+// import PGCR from "../components/PGCR";
+// import Error from "../components/Error";
+import Loading from "../components/Loading";
 import ReactGA from "react-ga";
 import "../styles/main.scss";
+
+import Loadable from "react-loadable";
+
+const AsyncHome = Loadable({
+	loader: () => import("../components/Home"),
+	loading: Loading
+});
+const AsyncRaid = Loadable({
+	loader: () => import("../components/Raid"),
+	loading: Loading
+});
+const AsyncGambit = Loadable({
+	loader: () => import("../components/Gambit"),
+	loading: Loading
+});
+const AsyncCrucible = Loadable({
+	loader: () => import("../components/Crucible"),
+	loading: Loading
+});
+const AsyncPGCR = Loadable({
+	loader: () => import("../components/PGCR"),
+	loading: Loading
+});
+const AsyncError = Loadable({
+	loader: () => import("../components/Error"),
+	loading: Loading
+});
 
 ReactGA.initialize("UA-125575293-2");
 
 const App = () => {
-  if (typeof window !== "undefined") {
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }
-  return (
-    <div className="App">
-      <NavBar />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/gambit/:id" component={Gambit} />
-        <Route exact path="/crucible/:id" component={Crucible} />
-        <Route exact path="/raid/:id" component={Raid} />
-        <Route exact path="/pgcr/:instanceId" component={PGCR} />
-        <Route component={Error} />
-      </Switch>
-      <Footer />
-    </div>
-  );
+	if (typeof window !== "undefined") {
+		ReactGA.pageview(window.location.pathname + window.location.search);
+	}
+	return (
+		<div className="App">
+			<NavBar />
+			<Switch>
+				<Route exact path="/" component={AsyncHome} />
+				<Route exact path="/gambit/:id" component={AsyncGambit} />
+				<Route exact path="/crucible/:id" component={AsyncCrucible} />
+				<Route exact path="/raid/:id" component={AsyncRaid} />
+				<Route exact path="/pgcr/:instanceId" component={AsyncPGCR} />
+				<Route component={AsyncError} />
+			</Switch>
+			<Footer />
+		</div>
+	);
 };
 
 // class App extends Component {
