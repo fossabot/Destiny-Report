84544d4e99ada4d695e277c55315ddee0bb332b8
Sarbast Mohammed <hi@sarbastmohammed.me>
2019-06-20 15:31:24 +0200

    Fixed activeClassName server and client mismatch (#50)

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,14 +1,20 @@
 import { withRouter } from "next/router";
 import Link from "next/link";
-import React, { Children } from "react";
+import React, { Children, useState, useEffect } from "react";
 import cx from "classnames";
 
 const ActiveLink = ({ router, activeClassName, children, ...props }) => {
   const child = Children.only(children);
 
-  const className = cx(child.props.className, {
-    [activeClassName]: router.asPath === props.as
-  });
+  const [className, setClassName] = useState("");
+
+  useEffect(() => {
+    const currentClassName = cx(child.props.className, {
+      [activeClassName]: router.asPath === props.as
+    });
+
+    setClassName(currentClassName);
+  }, []);
 
   return <Link {...props}>{React.cloneElement(child, { className })}</Link>;
 };
